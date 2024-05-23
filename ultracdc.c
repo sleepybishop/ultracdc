1a59f8cc7ef037c140d6077f2b51cf4e27b9f7af
Joe Calderon <calderon.joe@gmail.com>
2024-05-23 08:00:36 -0700

    add runtime lut

--- ultracdc.c
+++ ultracdc.c
@@ -10,6 +10,15 @@ static const uint64_t MASK_S = 0x2F;
 static const uint64_t MASK_L = 0x2C;
 static const uint64_t LEST = 64;
 
+static int8_t hdist[256][256];
+
+static void filltab(uint8_t patbyte)
+{
+    for (int i = 0; i < 256; i++)
+        for (int j = 0; j < 256; j++)
+            hdist[i][j] = __builtin_popcount(j ^ patbyte) - __builtin_popcount(i ^ patbyte);
+}
+
 static uint32_t normal_size(const uint32_t mi, uint32_t av, const uint32_t ma)
 {
     uint32_t ns = mi + (1 << 13);
@@ -20,6 +29,8 @@ static uint32_t normal_size(const uint32_t mi, uint32_t av, const uint32_t ma)
     return av;
 }
 
+//#define SLIDE(d, ob, ib) (d += __builtin_popcount((ib) ^ 0xAA) - __builtin_popcount((ob) ^ 0xAA))
+#define SLIDE(d, ob, ib) d += hdist[(ob)][(ib)]
 static uint32_t cut(const uint8_t *src, const uint32_t len, const uint32_t mi, const uint32_t ma, uint32_t ns)
 {
     uint32_t n = len, cnt = 0;
@@ -44,9 +55,7 @@ static uint32_t cut(const uint8_t *src, const uint32_t len, const uint32_t mi, c
             for (uint32_t j = 0; j < 8; j++) {
                 if ((dist & ((i < ns) ? MASK_S : MASK_L)) == 0)
                     return i + 8;
-                //SLIDE(dist);
-                dist -= __builtin_popcount(src[i + j - 8] ^ 0xAA);
-                dist += __builtin_popcount(src[i + j] ^ 0xAA);
+                SLIDE(dist, src[i + j - 8], src[i + j]);
             }
             owin = iwin;
         }
@@ -60,6 +69,7 @@ cdc_ctx ultracdc_init(uint32_t mi, uint32_t av, uint32_t ma)
     ma = ULTRACDC_CLAMP(ma, av, MAX_CHUNK_SIZE);
     uint32_t ns = normal_size(mi, av, ma);
     cdc_ctx ctx = {.mi = mi, .ma = ma, .ns = ns, .pos = 0};
+    filltab(PATTERN & 0xff);
     return ctx;
 }
 
